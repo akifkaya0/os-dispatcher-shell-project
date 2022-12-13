f992f1bd9ebb4687f4054fff7a8f9048c270f095
alperenmemur <alperen.memur@hotmail.com>
2022-12-13 19:19:17 +0300

    Dosyadan okuma

--- src/main/java/com.os/main.java
+++ src/main/java/com.os/main.java
@@ -1,9 +1,23 @@
 package src.main.java.com.os;
+import java.io.FileReader;
+import java.io.BufferedReader;
+import java.io.IOException;
 
 public class main {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         //Enes Bekkaya
+
+        //  Path path = Paths.get(args[0]);
+        //  String okunacakDosya = path.toAbsolutePath().toString();
+
+        FileReader fr = new FileReader("C:\\Users\\HP\\Desktop\\giriş.txt");
+        String line;
+        BufferedReader br = new BufferedReader(fr);
+
+        while((line = br.readLine()) != null) {
+            System.out.println(line);
+        }
 
     }
 }
