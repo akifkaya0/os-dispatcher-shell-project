8a45a08c3b4e99c523b5fb97b0e1c197c6f79cb7
Mehmet Akif Kaya <82850894+akifkaya0@users.noreply.github.com>
2022-12-20 01:16:48 +0300

    proses ekrana yazdırma fonksiyonları eklendi

--- src/main/java/com.os/Proses.java
+++ src/main/java/com.os/Proses.java
@@ -1,15 +1,33 @@
 package src.main.java.com.os;
 
 public class Proses {
+
+    private int prosesId;
     private int varisZamani;
     private int oncelik;
     private int prosesZamani;
-    public Proses(int varisZamani, int oncelik, int prosesZamani) {
+
+
+
+    public Proses(int prosesId, int varisZamani, int oncelik, int prosesZamani) {
         this.varisZamani = varisZamani;
         this.oncelik = oncelik;
         this.prosesZamani = prosesZamani;
+        this.prosesId = prosesId;
 
     }
+
+    public void printProses(float dispatcherTimer , String printType){
+
+        String information = String.format("%.4f", dispatcherTimer) + " sn proses " + printType;
+        String processId = "(id:"+ String.format("%04d", this.getProsesId());
+        String processOncelik = " öncelik:"+ this.getOncelik();
+        String processArrivalTime = " kalan süre:"+ this.getProsesZamani() +" sn)";
+
+        System.out.format("%20s  %10s  %10s  %10s  \n" , information, processId , processOncelik , processArrivalTime);
+
+    }
+
 
     public int getVarisZamani() {
         return varisZamani;
@@ -33,5 +51,13 @@ public class Proses {
 
     public void setProsesZamani(int prosesZamani) {
         this.prosesZamani = prosesZamani;
+    }
+
+    public int getProsesId() {
+        return prosesId;
+    }
+
+    public void setProsesId(int prosesId) {
+        this.prosesId = prosesId;
     }
 }
